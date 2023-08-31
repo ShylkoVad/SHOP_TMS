0f6fc6a0f2e3ffe1674ee71d2300944dca97b2bc
Vadzim <shylkovad@gmail.com>
2023-08-31 20:39:03 +0300

    master

---
+++ src/test/java/com/example/shop_tms/ShopTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
